b8a85a3975f2b8e7f5844fa7663da2f3bb078295
AdrienCastelbou <acastelbou1@gmail.com>
2020-11-30 12:14:50 +0100

    %x, %X and read all first input ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acastelb <acastelb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 11:38:11 by acastelb          #+#    #+#             */
-/*   Updated: 2020/11/27 18:32:46 by acastelb         ###   ########.fr       */
+/*   Updated: 2020/11/30 12:12:48 by acastelb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,37 @@ char		*ft_utoa(long int n)
 	return (str);
 }
 
+char	*ft_convert_hex(long int nb, char *base)
+{
+	int size;
+	int is_neg;
+	long int n;
+	char *str;
+
+	is_neg = 0;
+	n = nb;
+	size = 1;
+	if (n < 0)
+	{
+		size += 1;
+		n *= -1;
+	}
+	while (n >= 16)
+	{
+		n /= 16;
+		size += 1;
+	}
+	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	str[size] = '\0';
+	while (size--)
+	{
+		str[size] = base[nb % 16];
+		nb /= 16;
+	}
+	return (str);
+}
+
 int		ft_conversion(char *s, va_list ap, t_list **to_print)
 {
 	int		i;
@@ -85,20 +116,33 @@ int		ft_conversion(char *s, va_list ap, t_list **to_print)
 			str = ft_itoa(va_arg(ap, int));
 		else if (s[i] == 'u')
 			str = ft_utoa(va_arg(ap, unsigned int));
+		else if (s[i] == 'x')
+			str = ft_convert_hex(va_arg(ap, unsigned int), "0123456789abcdef");
+		else if (s[i] == 'X')
+			str = ft_convert_hex(va_arg(ap, unsigned int), "0123456789ABCDEF");
+		else if (s[i] == '%')
+			str = strdup("%");
+		if (str == NULL)
+			return (-1);
 		new = ft_lstnew(str);
 		ft_lstadd_back(to_print, new);
 	}
-	return (1);
+	return (i);
 }
 
-void	ft_print_all(t_list *to_print)
+int		ft_print_all(t_list *to_print)
 {
+	int len;
+
+	len = 0;
 	while (to_print)
 	{
 		if (to_print->content)
 			ft_putstr_fd((char *)to_print->content, 1);
+		len += ft_strlen((char *)to_print->content);
 		to_print = to_print->next;
 	}
+	return (len);
 }
 
 int		ft_printf(const char *s, ...)
@@ -107,25 +151,41 @@ int		ft_printf(const char *s, ...)
 	t_list	*to_print;
 	char		*tmp;
 	int			i;
+	int j;
 
 	va_start(ap, s);
-	i = -1;
-	while (s[++i])
-		if (s[i] == '%')
+	i = 0;
+	j = -1;
+	while (s[++j])
+		if (s[j] == '%')
 		{
-			tmp = strndup(s, i);
-			to_print = ft_lstnew((void *)tmp);
-			ft_conversion((char *)&s[i + 1], ap, &to_print);
+			tmp = strndup(s + i, j - i);
+			ft_lstadd_back(&to_print, ft_lstnew((void *)tmp));
+			i = j;
+			if ((j = ft_conversion((char *)&s[i += 1], ap, &to_print)) == -1)
+			{
+				ft_lstclear(&to_print, free);
+				return (-1);
+			}
+			i+= 1;
+			j += i;
 		}
-	ft_print_all(to_print);
+	if (j > i)
+	{
+		tmp = strndup(s + i, j - i);
+		ft_lstadd_back(&to_print, ft_lstnew((void *)tmp));
+	}
+	i = ft_print_all(to_print);
 	ft_lstclear(&to_print, free);
 	va_end(ap);
-	return (1);
+	return (i);
 }
 
 
 int main(int ac, char **av)
 {
-	ft_printf("hello %u", atoi(av[1]));
-	printf("hello %u", atoi(av[1]));
+	int a = 200;
+
+	ft_printf("a%db", 1);
+
 }
